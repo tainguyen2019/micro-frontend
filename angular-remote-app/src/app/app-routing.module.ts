d48e9c7d28370a57285c681becbbca786ba2b10c
tainguyen2019 <tainguyen.uit@gmail.com>
2025-01-06 18:25:41 +0700

    deploy: angular remote app

--- angular-remote-app/src/app/app-routing.module.ts
+++ angular-remote-app/src/app/app-routing.module.ts
@@ -1,25 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { environment } from 'src/environments/environment';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 
-const BASE_URL = 'angular-app';
-const computeRoute = (path: string) => environment.production ? path : `${BASE_URL}/${path}`
-
 const routes: Routes = [
   {
-    path: computeRoute('home'),
+    path: 'home',
     component: HomeComponent
   },
   {
-    path: computeRoute('about'),
+    path: 'about',
     component: AboutComponent
   },
   {
     path: '**',
-    redirectTo: computeRoute('home'),
-
+    redirectTo: 'home'
   }
 ];
 
