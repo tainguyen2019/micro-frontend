162083699a724407f14b5c8c24589b57d90014d5
tainguyen2019 <tainguyen.uit@gmail.com>
2025-01-06 22:58:33 +0700

    deploy: micro frontend page

--- react-remote-app/webpack.config.js
+++ react-remote-app/webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif|svg|ico|json)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
